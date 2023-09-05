183711b243f47491d2c46e9b3980307aa0f892af
AbdallaSalah200 <142856930+AbdallaSalah200@users.noreply.github.com>
2023-09-06 00:37:18 +0300

    Add font famaily

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:e_book_app/Feature/Splash/persentation/views/splash_view.dart';
 
 void main() {
@@ -13,7 +14,13 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return   GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const  Color(0xff100B20)),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const  Color(0xff100B20),
+      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+      
+      )
+    
+      ,
+    
       home:const  SplashView(),
     );
   }
