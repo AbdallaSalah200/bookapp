a4c8262bdc1956a20041a861b167acc6f690ae41
AbdallaSalah200 <142856930+AbdallaSalah200@users.noreply.github.com>
2023-09-06 03:41:18 +0300

    styles part 2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:e_book_app/constant.dart';
 
 abstract class Styles {
   static const textStyle18 =TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600
+  );
+  static const textStyle20 =TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kFont
+  );
+  static const textStyle14 =TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal
+  );
+  static const textStyle16 =TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500
+  );
+  static const textStyle30 =TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kFont
   );
 }
