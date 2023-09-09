3b0cb0b88aba67a660907a8ce13b252761e702dd
AbdallaSalah200 <142856930+AbdallaSalah200@users.noreply.github.com>
2023-09-09 03:57:59 +0300

    create class fauilar&methods home repo

--- lib/Feature/home/data/repo/home_repo.dart
+++ lib/Feature/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_book_app/core/errors/fauiler.dart';
+import 'package:e_book_app/Feature/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo{
-  fetchBestSellerBooks ();
-  fetchFeaturedBooks ();
+  Future<Either<Fauilar,List<BookModel>>>   fetchBestSellerBooks ();
+  Future<Either<Fauilar,List<BookModel>>>   fetchFeaturedBooks ();
 }
