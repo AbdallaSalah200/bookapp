4844e8503440934773da79b0feb1acc48453a05d
AbdallaSalah200 <142856930+AbdallaSalah200@users.noreply.github.com>
2023-09-10 06:30:29 +0300

    create feature book methods

--- lib/Feature/home/data/presentiaion/mangers/feature_book_cubit/feture_book_cubit.dart
+++ lib/Feature/home/data/presentiaion/mangers/feature_book_cubit/feture_book_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:e_book_app/Feature/home/data/repo/home_repo.dart';
 import 'package:e_book_app/Feature/home/data/models/book_model/book_model.dart';
 
 part 'feture_book_state.dart';
 
 class FetureBookCubit extends Cubit<FetureBookState> {
-  FetureBookCubit() : super(FetureBookInitial());
+  FetureBookCubit(this.homeRepo) : super(FetureBookInitial());
+  final HomeRepo homeRepo ;
+  Future <void> fetchFeaturedBooks () async {
+    emit(FetureBookLodaing());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((fauiler) {
+      emit(FetureBookFailure(fauiler.errmessage));
+    }, (books) {
+      emit(FetureBookSuccess(books));
+    });
+  }
 }
